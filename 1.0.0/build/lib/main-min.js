16b9eea797381589e93c7d7b9bdd8481d9b75def
kissyteam <tiehang.lth@taobao.com>
2015-03-27 14:32:55 +0800

    sync daily version 1.0.0

---
+++ 1.0.0/build/lib/main-min.js
@@ -0,0 +1,1 @@
+KISSY.add("kg/effect-popup/1.0.0/lib/main",["node","base"],function(e,n,i,a){var o=(n("node").all,n("base"));a.exports=o.extend({initializer:function(){}},{ATTRS:{}})});
---
+++ 1.0.0/build/mods-min.js
@@ -0,0 +1,1 @@
+KISSY.config("requires",{"kg/effect-popup/1.0.0/index":["node"],"kg/effect-popup/1.0.0/lib/main":["node","base"]});
